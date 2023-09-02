246a204cf2e45be2ed0087ec058917fd2528f594
marionatuhwo <ano@hawaii.edu>
2023-09-01 17:02:14 -1000

    Completed Part 8 On Your Own Section

--- HackwichTwo_Test/ViewController.swift
+++ HackwichTwo_Test/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var myNameis = "My name is "
+    var myFullName = "Marion Ano"
+    var fullName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fullName = myNameis + myFullName
     }
 
-
+    @IBAction func pressMeButtonPressed(_ sender: Any) {
+        print("Hello World")
+    
+    }
+    
+    @IBAction func otherButtonPressed(_ sender: Any) {
+        print(fullName)
+    }
 }
 
